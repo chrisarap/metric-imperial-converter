9bb43199a628605709d66e009431367cd2a1eb61
chrisarap <carteaga460@gmail.com>
2022-07-11 18:31:34 -0400

    logic added to the route /api/convert

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,54 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+
+  this.getNum = input => Number(input.match(/\d+.\d+/).join(''));
+
+  this.getUnit = input => (input.match(/[a-zA-Z]/g).join('') == 'l') ? 'L' : input.match(/[a-zA-Z]/g).join('');
+
+  this.getReturnUnit = initUnit => {
+    if (initUnit == 'mi') return 'km';
+    if (initUnit == 'km') return 'mi';
+
+    if (initUnit == 'lbs') return 'kg';
+    if (initUnit == 'kg') return 'lbs';
+
+    if (initUnit == 'gal') return 'L';
+    if (initUnit == 'l' || initUnit == 'L') return 'gal';
   };
 
   this.spellOutUnit = function(unit) {
     let result;
-    
+
     return result;
   };
-  
+
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
-    
-    return result;
+
+    if (initUnit == 'gal')  return Math.round(initNum * galToL * 100000) / 100000;
+    if (initUnit == 'l' || initUnit == 'L')    return Math.round(initNum / galToL * 100000) / 100000;
+
+    if (initUnit == 'lbs')  return Math.round(initNum * lbsToKg * 100000) / 100000;
+    if (initUnit == 'kg')  return Math.round(initNum / lbsToKg * 100000) / 100000;
+
+    if (initUnit == 'mi')  return Math.round(initNum * miToKm * 100000) / 100000;
+    if (initUnit == 'km')  return Math.round(initNum / miToKm * 100000) / 100000;
   };
-  
+
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+    const units = {
+      mi: 'miles',
+      km: 'kilometers',
+      gal: 'gallons',
+      l: 'liters',
+      L: 'liters',
+      lbs: 'pounds',
+      kg: 'kilograms'
+    }
+
+    return `${initNum} ${units[initUnit]} converts to ${returnNum} ${units[returnUnit]}`;
   };
-  
 }
 
 module.exports = ConvertHandler;
