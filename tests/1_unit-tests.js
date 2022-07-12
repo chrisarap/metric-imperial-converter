3b7ef43e8028632fec0ca5152984baaa569fbb33
chrisarap <carteaga460@gmail.com>
2022-07-12 11:55:58 -0400

    first test added

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,9 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
-
+  test('Testing whole number input', ()=> {
+    assert.strictEqual(convertHandler.getNum('5mi'), 5, "whole number read correctly");
+    assert.strictEqual(convertHandler.getNum('8gal'), 8, "whole number read correctly");
+    assert.strictEqual(convertHandler.getNum('12l'), 12, "whole number read correctly");
+  });
 });
